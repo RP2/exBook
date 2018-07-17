6bec6a0bc93bd5dc85c5bdeef74580f33c454767
Riley <RP2@users.noreply.github.com>
2018-07-17 11:10:32 -0700

    app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,16 @@
+$(document).ready(function() {
+  console.log('app.js loaded!');
+
+$.ajax({
+    method: 'GET',
+    url: '/api/users',
+    success: handleSuccess,
+    error: handleError
+  });
+
  var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    enter: {lat: -34.397, lng: 150.644}, zoom: 8});
+  };
+};
