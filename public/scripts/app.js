1a194eb14cb7390838ab4116cccd0a0b54d2daa1
Riley <RP2@users.noreply.github.com>
2018-07-17 11:43:27 -0700

    fixed map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,7 @@
+var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+
 $(document).ready(function() {
   console.log('app.js loaded!');
-
-
-// $.ajax({
-//     method: 'GET',
-//     url: '/api/users',
-//     success: handleSuccess,
-//     error: handleError
-//   });
 
 $('#loginButton').on('click', function(){
   event.preventDefault();
@@ -19,40 +13,58 @@ $('.close').on('click', function(){
     $('#loginModal').hide();
   });
 
-// mark user's location in the map
-var map, infoWindow;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 6
-        });
-        infoWindow = new google.maps.InfoWindow;
+  $.ajax({
+    url: weekly_quakes_endpoint,
+    method: 'GET',
+    success: mapSuccess,
+    error: mapError,
+  });
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+  var mapTitle;
+  var thePosition;
+  var image = {
+    url: 'https://git.generalassemb.ly/sf-wdi-46/jquery-geoquakes-lab/blob/master/images/earthquake.png?raw=true',
+    size: new google.maps.Size(20,32),
+    origin: new google.maps.Point(0,0),
+    anchor: new google.maps.Point(0,0),
+    scaledSize: new google.maps.Size(25,25)
+  };
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      };
+function mapSuccess(responce){
+  for (let i = 0; i < responce.features.length; i++){
+      mapTitle = responce.features[i].properties.place
+      $('#info').append('<p>' + mapTitle + '</p>');
+    };
+  console.log(responce);
+  // marker(responce);
+  initMap(responce);
+};
+
+var map;
+
+function initMap(responce){
+  map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 37.78, lng: -122.44},
+          zoom: 2
+});
+
+  for (var i = 0; i < responce.features.length; i++){
+    var pinLocation = responce.features[i].geometry.coordinates;
+    var LatLng = new google.maps.LatLng(pinLocation[1], pinLocation[0]);
+
+    var marker = new google.maps.Marker({
+      position: LatLng,
+      map: map,
+      icon: image
+    });
+  };
+};
+
+
+function mapError(error1, error2, error3){
+    console.log(error1);
+    console.log(error2);
+    console.log(error3);
+  };
+});
