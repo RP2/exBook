bad21a45bd3fc59b8e99d0ffaf50b3b377709c8e
Riley <RP2@users.noreply.github.com>
2018-07-17 11:28:27 -0700

    add login modal

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,28 @@
 $(document).ready(function() {
   console.log('app.js loaded!');
 
-$.ajax({
-    method: 'GET',
-    url: '/api/users',
-    success: handleSuccess,
-    error: handleError
+
+// $.ajax({
+//     method: 'GET',
+//     url: '/api/users',
+//     success: handleSuccess,
+//     error: handleError
+//   });
+
+$('#loginButton').on('click', function(){
+  event.preventDefault();
+    $('#loginModal').show();
+    // $('#modalImg').attr('src', "./images/MG2.jpg");
+    $('#modalP').html(`<p>Login or Sign up</p>`);
+});
+$('.close').on('click', function(){
+    $('#loginModal').hide();
   });
 
- var map;
+
   function initMap() {
+    var map;
     map = new google.maps.Map(document.getElementById('map'), {
     enter: {lat: -34.397, lng: 150.644}, zoom: 8});
   };
-};
+});
