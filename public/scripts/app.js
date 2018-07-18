df4a7e84e0ef60faae258d36f8b97ac55eca375e
Riley <RP2@users.noreply.github.com>
2018-07-18 09:43:31 -0700

    added results

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,9 @@ var book_endpoint = "http://localhost:3000/api/users/"
 
 $(document).ready(function() {
   console.log('app.js loaded!');
-
+///////////////////////////////////////////
+//////////login button function////////////
+///////////////////////////////////////////
 $('#loginButton').on('click', function(){
   event.preventDefault();
     $('#loginModal').show();
@@ -12,7 +14,9 @@ $('#loginButton').on('click', function(){
 $('.close').on('click', function(){
     $('#loginModal').hide();
   });
-
+////////////////////////////////////////////
+//////////signup button function////////////
+////////////////////////////////////////////
 $('#signupButton').on('click', function(){
   event.preventDefault();
     $('#signupModal').show();
@@ -24,7 +28,9 @@ $('#signupButton').on('click', function(){
 $('.close').on('click', function(){
     $('#signupModal').hide();
   });
-
+////////////////////////////////////////////
+//////////post button function//////////////
+////////////////////////////////////////////
 $('#postButton').on('click', function(){
   event.preventDefault();
   $('#postModal').show();
@@ -32,7 +38,9 @@ $('#postButton').on('click', function(){
 $('.close').on('click', function(){
     $('#postModal').hide();
   });
-
+////////////////////////////////////////////
+//////////profile button function///////////
+////////////////////////////////////////////
 $('#profileButton').on('click', function(){
   event.preventDefault();
   $('#profileModal').show();
@@ -41,7 +49,42 @@ $('.close').on('click', function(){
     $('#profileModal').hide();
   });
 
-//begin map code
+/////////////////////begin post function code//////////////////////
+
+$('#postForm').on('submit', function(e){
+  e.preventDefault();
+  var formData = $(this).serialize();
+  $.post('/api/users/:user_id/posts', formData, function(post){
+    renderPost(post);
+  })
+  $(this).trigger('reset');
+});
+
+  // add click handler to 'add post' buttons
+  $('#posts').on('click', '.add-post', function(e) {
+    console.log('add-post clicked!');
+
+    var id = $(this).closest('.post').data('post-id');
+    console.log('id', id);
+
+    $('#postModal').data('post-id', id);
+    $('#postModal').modal();
+  });
+
+  $('#posts').on('click', '.delete-post', function(e) {
+    var id = $(this).closest('.post').data('post-id');
+    console.log('id', id);
+
+    $.ajax({
+      url: '/api/users/:user_id/posts' + id,
+      type: 'DELETE',
+      success: function(result) {
+        $('[data-post-id=' + id + ']').remove();
+      }
+    });
+  });
+
+//////////////////////////begin map code//////////////////////////
   $.ajax({
     url: book_endpoint,
     method: 'GET',
@@ -61,10 +104,11 @@ $('.close').on('click', function(){
 
 
 function mapSuccess(responce){
-  // for (let i = 0; i < responce.features.length; i++){
-  //     mapTitle = responce.features[i].properties.place
-  //     $('#info').append('<p>' + mapTitle + '</p>');
-  //   };
+  for (let i = 0; i < responce.length; i++){
+      mapTitle = responce[i].posts[i].title;
+      mapPrice = responce[i].posts[i].price;
+      $('#info').append('<p>' + mapTitle + ', $' + mapPrice + '</p>');
+    };
   console.log(responce);
   // marker(responce);
   initMap(responce);
