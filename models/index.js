bccbf0f26b01dc87958d1d7f7ef92c4ca0d03288
laynahgao <laynahgao@yahoo.com>
2018-07-20 17:09:09 -0700

    db

--- models/index.js
+++ models/index.js
@@ -1,6 +1,10 @@
 // require mongoose and connect to database
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/exBookTest', { useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost:27017/exBookTest', { useNewUrlParser: true });
+// heroku db
+var uri = 'mongodb://heroku_2nzq3ptj:rft13d0fv7otomskr09vsoihfs@ds145951.mlab.com:45951/heroku_2nzq3ptj';
+
+var options = { useNewUrlParser: true };
 
 let User = require('./user');
 let Post = require('./post')
@@ -9,3 +13,9 @@ module.exports = {
   User: User,
   Post: Post
 };
+
+mongoose.connect(uri, options);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
